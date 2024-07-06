abf060cbbc486e3f409241963bd9c8a9d7cfb312
NikolinaS1 <nikolina.stjepanovic1@gmail.com>
2024-07-06 20:00:23 +0200

    Fixed css bug with delete icon

--- src/app/components/post/post.module.ts
+++ src/app/components/post/post.module.ts
@@ -4,10 +4,17 @@ import { PostComponent } from './post.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
   declarations: [PostComponent],
-  imports: [CommonModule, MatIconModule, MatMenuModule, MatSnackBarModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSnackBarModule,
+    MatBadgeModule,
+  ],
   exports: [PostComponent],
 })
 export class PostModule {}
