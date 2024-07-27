e85fa36556268a8a5447ceae0c88297dba65c6af
NikolinaS1 <nikolina.stjepanovic1@gmail.com>
2024-07-27 23:02:13 +0200

    Chat interface

--- src/app/pages/chat/services/chat.service.ts
+++ src/app/pages/chat/services/chat.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+import { ChatMessage } from '../models/chat.model';
 
 @Injectable({
   providedIn: 'root',
@@ -16,7 +17,7 @@ export class ChatService {
     try {
       const timestamp = new Date();
 
-      const messageData = {
+      const messageData: ChatMessage = {
         senderId,
         receiverId,
         message,
@@ -34,12 +35,12 @@ export class ChatService {
     }
   }
 
-  getMessages(senderId: string, receiverId: string): Observable<any[]> {
+  getMessages(senderId: string, receiverId: string): Observable<ChatMessage[]> {
     const chatPath = `chat/${senderId}_${receiverId}/messages`;
     const chatPathReverse = `chat/${receiverId}_${senderId}/messages`;
 
     return this.firestore
-      .collection(chatPath, (ref) => ref.orderBy('timestamp'))
+      .collection<ChatMessage>(chatPath, (ref) => ref.orderBy('timestamp'))
       .valueChanges();
   }
 }
